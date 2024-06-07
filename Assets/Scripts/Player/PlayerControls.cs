bb627d3095b1f104fc826135b001cca3c1946ec4
Allen henry Bautista <77380231+AllenHenrybautista@users.noreply.github.com>
2024-06-07 15:52:39 +0800

    Refactor Player Controller Script
    Merged Animation Controller Script and Input Movement System

--- Assets/Scripts/Player/PlayerControls.cs
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -6,28 +6,60 @@ using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
-
     [SerializeField] private float _speed = 5f;
 
+    //Input System
+    public static Vector2 Movement;
     private Vector2 _movement;
-
     private Rigidbody2D _rb;
-    private Animator _animator; 
+    private Animator _animator;
+    private InputAction _moveAction;
+    private PlayerInput _playerInput;
 
+    //Animation Parameters
     private const string Horizontal = "Horizontal";
     private const string Vertical = "Vertical";
     private const string LastHorizontal = "LastHorizontal";
     private const string LastVertical = "LastVertical";
 
+
     private void Awake()
+    {
+        SetupPlayer();
+        ReadInputs();
+    }
+
+    private void Update()
+    {
+        PlayerMove();
+        HandleAnimation();
+    }
+
+    private void SetupPlayer()
     {
         _rb = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
     }
 
-    private void Update()
+    private void ReadInputs()
     {
-         _movement.Set(PlayerControl.Movement.x, PlayerControl.Movement.y);
+        _playerInput = GetComponent<PlayerInput>();
+        if (_playerInput != null)
+            _moveAction = _playerInput.actions["Move"];        
+        else
+            Debug.LogError("PlayerInput is Missing");
+    }
+
+    private void PlayerMove()
+    {
+        if (_moveAction != null)
+            Movement = _moveAction.ReadValue<Vector2>();
+
+    }
+
+    private void HandleAnimation()
+    {
+        _movement.Set(Movement.x, Movement.y);
         _rb.velocity = _movement * _speed;
         _animator.SetFloat(Horizontal, _movement.x);
         _animator.SetFloat(Vertical, _movement.y);
